d2f0a2a62efbd6882ad87ca7fb1964dc616773f2
Liz Ing-Simmons (k2474365) <liz.ing-simmons@kcl.ac.uk>
2025-08-01 11:28:34 +0100

    feat: move utility functions to single utils.py

--- bin/utils.py
+++ bin/utils.py
@@ -1,4 +1,24 @@
+import pandas as pd
 import subprocess
+
+ESTIMATE_VALUE = 10343.0727848101
+
+
+def read_node_data(csv_path):
+    df = pd.read_csv(csv_path)
+    node_carbon = {
+        row["node_name"]: {
+            "cpu_cores": row["cpu_cores"],
+            "embodied": row["embodied_carbon_kgco2e"],
+            "embodied_estimate": round(row["embodied_carbon_kgco2e"], 6) == round(ESTIMATE_VALUE, 6),
+            "max_power_draw": row["energy_100pct_kw"],
+            "min_power_draw": row["energy_0pct_kw"],
+            "model_id": row["model_manufacturer_id"]
+        }
+        for _, row in df.iterrows()
+    }
+    return node_carbon
+
 
 def get_sacct_tokens(jobid):
     """Fetch sacct data and return tokens for the step with energy data."""
