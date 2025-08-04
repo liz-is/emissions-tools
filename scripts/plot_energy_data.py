a36f4e1d32565abbf24feb4854ca6638ce696ea1
Liz Ing-Simmons (k2474365) <liz.ing-simmons@kcl.ac.uk>
2025-08-04 16:45:20 +0100

    feat: use actual elapsed time to calculate energy usage per second

--- scripts/plot_energy_data.py
+++ scripts/plot_energy_data.py
@@ -20,13 +20,13 @@ energy_data[["node", "cores", "time"]] = energy_data["JobName"].str.extract(
 
 energy_data["cores"] = energy_data["cores"].astype(int)
 energy_data["time"] = energy_data["time"].astype(int)
-energy_data["joules_per_min"] = energy_data["ConsumedEnergyRaw"] / energy_data["time"]
+energy_data["joules_per_s"] = energy_data["ConsumedEnergyRaw"] / (energy_data["ElapsedRaw"])
 
 # plot
 ax = sns.barplot(
     data=energy_data,
     x="cores",
-    y="joules_per_min",
+    y="joules_per_s",
     hue="node",
     errorbar="sd",
     err_kws={"color": "black", "linewidth": 1.5},
@@ -38,7 +38,7 @@ ax = sns.barplot(
 sns.stripplot(
     data=energy_data,
     x="cores",
-    y="joules_per_min",
+    y="joules_per_s",
     hue="node",
     dodge=True,
     alpha=0.6,
