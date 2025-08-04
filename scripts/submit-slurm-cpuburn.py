dd8a24f5b1ec463cfa8e2d9fa69ae903526c94b0
Liz Ing-Simmons (k2474365) <liz.ing-simmons@kcl.ac.uk>
2025-08-04 10:06:49 +0100

    feat: add sleep command for 0 cores

--- scripts/submit-slurm-cpuburn.py
+++ scripts/submit-slurm-cpuburn.py
@@ -18,14 +18,18 @@ fixed_sbatch_options = [
 
 for node in node_info.keys():
     node_cpus = node_info[node]["max_cpus"]
-    n_cpus = [1] + [int(node_cpus * n) for n in cpu_fraction]
+    n_cpus = [0, 1] + [int(node_cpus * n) for n in cpu_fraction]
 
     for n in n_cpus:
         for t in burn_time_min:
             job_name = f"{node}_{n}c_{t}m"
 
             time_s = 60*t
-            cmd = f"/scratch/prj/emissions/cpumemburn --cpuburn={n} --burntime={time_s}"
+            
+            if n == 0:
+                cmd = f"sleep {time_s}"
+            else:
+                cmd = f"/scratch/prj/emissions/cpumemburn --cpuburn={n} --burntime={time_s}"
 
             sbatch_options = [
                 f"--nodelist={node}",
@@ -43,4 +47,29 @@ for node in node_info.keys():
 
                 subprocess.run(["sbatch", temp_file.name])
                 print(f"Submitted {job_name}")
+
+    # zero cores = sleep
+    for t in burn_time_min:
+        n = 0
+        job_name = f"{node}_{n}c_{t}m"
+
+        time_s = 60*t
+        cmd = f"sleep {time_s}"
+
+        sbatch_options = [
+            f"--nodelist={node}",
+            f"--cpus-per-task={node_cpus}",
+            f"--job-name={job_name}",
+            f"--output={job_name}_%j.out",
+            f"--time=00:{t}:10"
+        ]
+
+        all_sbatch_options = "\n#SBATCH " + "\n#SBATCH ".join(fixed_sbatch_options + sbatch_options)
+
+        with tempfile.NamedTemporaryFile(mode='w+', delete=False) as temp_file:
+            temp_file.write("#!/bin/bash\n\n" + all_sbatch_options + "\n\n" + cmd + "\n")
+            temp_file.flush()  # Ensure data is written
+
+            subprocess.run(["sbatch", temp_file.name])
+            print(f"Submitted {job_name}")
 
