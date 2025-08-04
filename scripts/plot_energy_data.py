8de5f57b6828cd0184acf2373483eb72ae824a50
Liz Ing-Simmons (k2474365) <liz.ing-simmons@kcl.ac.uk>
2025-08-04 12:10:47 +0100

    feat: add script to plot energy data from csv file

---
+++ scripts/plot_energy_data.py
@@ -0,0 +1,51 @@
+import argparse
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "file",
+    help="Input data file in .csv format, with columns 'JobID', 'JobName', 'ConsumedEnergyRaw'",
+)
+parser.add_argument("--plot", default="plot.png", help="Output plot file name.")
+args = parser.parse_args()
+
+energy_data = pd.read_csv(args.file)
+
+energy_data[["node", "cores", "time"]] = energy_data["JobName"].str.extract(
+    r"(.*)_(\d{1,2})c_(\d{1,2})m"
+)
+
+energy_data["cores"] = energy_data["cores"].astype(int)
+energy_data["time"] = energy_data["time"].astype(int)
+energy_data["joules_per_min"] = energy_data["ConsumedEnergyRaw"] / energy_data["time"]
+
+# plot
+ax = sns.barplot(
+    data=energy_data,
+    x="cores",
+    y="joules_per_min",
+    hue="node",
+    errorbar="sd",
+    err_kws={"color": "black", "linewidth": 1.5},
+    edgecolor="black",
+    capsize=0.1,
+    alpha=0.5,
+)
+
+sns.stripplot(
+    data=energy_data,
+    x="cores",
+    y="joules_per_min",
+    hue="node",
+    dodge=True,
+    alpha=0.6,
+    ax=ax,
+    legend=False,
+)
+
+sns.move_legend(ax, "lower right")
+
+plt.savefig(args.plot)
